96351b49d3ecf9ac81cd8b6b68c8e49ee825697e
Francisco Posada <ciscotomaramo@gmail.com>
2024-05-10 15:10:26 +0100

    User specific views established in navbar for navigation purposes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,20 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# Destroys Users when rails db:seed is run
+User.destroy_all
+# Create the users array
+users = [
+  { email: 'cisco_user@gmail.com', password: 'password', is_admin: false },
+  { email: 'cisco_admin@gmail.com', password: 'password', is_admin: true }
+]
+
+# Iterate through users array and create instances of seed user data
+users.each do |user|
+  User.create!(
+    email: user[:email],
+    password: user[:password],
+    is_admin: user[:is_admin]
+  )
+end
+
+puts 'Destroying previous users ...'
+puts 'Creating users ... '
+puts 'Users created succesfully! '
